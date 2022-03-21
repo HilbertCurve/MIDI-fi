e0a65e7253d5aaf9f49ce3be17f8a78ab420aba3
Ben C <80780490+HilbertCurve@users.noreply.github.com>
2022-03-21 09:46:03 -0400

    rework wip

---
+++ src/Application.hpp
@@ -0,0 +1,14 @@
+/* date = March 21 2022 9:27 am */
+
+#ifndef MIDIFI_APPLICATION_HPP
+#define MIDIFI_APPLICATION_HPP
+
+namespace MidiFi {
+    void init();
+    // No scenes until UI stuff is finished
+    //void setScene(Scene *s);
+    //Scene &getScene();
+    void loop();
+}
+
+#endif /* MIDIFI_APPLICATION_HPP */
